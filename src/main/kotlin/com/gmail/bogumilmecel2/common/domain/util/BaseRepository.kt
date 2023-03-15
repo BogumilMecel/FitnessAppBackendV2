068cd115c8925c4ff4c7a5868e0ee4e071a890b6
Bogumił Męcel <Bodziowaty6978@gmail.com>
2023-03-15 15:05:00 +0100

    Implemented handle request method

--- src/main/kotlin/com/gmail/bogumilmecel2/common/domain/util/BaseRepository.kt
+++ src/main/kotlin/com/gmail/bogumilmecel2/common/domain/util/BaseRepository.kt
@@ -4,6 +4,15 @@ import com.gmail.bogumilmecel2.common.domain.model.CustomException
 import com.gmail.bogumilmecel2.common.util.Resource
 
 open class BaseRepository() {
+
+    suspend inline fun <R> handleRequest(block: () -> R): Resource<R> {
+        return try {
+            Resource.Success(block())
+        } catch (e: Exception) {
+            handleExceptionWithResource(exception = e)
+        }
+    }
+
     fun <T> handleExceptionWithResource(exception: Exception, data: T? = null): Resource<T> {
         exception.printStackTrace()
         return Resource.Error(
