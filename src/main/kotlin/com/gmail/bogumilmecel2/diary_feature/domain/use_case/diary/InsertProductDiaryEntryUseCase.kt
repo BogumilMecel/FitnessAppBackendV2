1c8aa068b89ea48725761716cdc8f51bdf5768d2
Bogumił Męcel <Bodziowaty6978@gmail.com>
2023-06-22 17:27:25 +0200

    Added date check in insert product diary entry use case

--- src/main/kotlin/com/gmail/bogumilmecel2/diary_feature/domain/use_case/diary/InsertProductDiaryEntryUseCase.kt
+++ src/main/kotlin/com/gmail/bogumilmecel2/diary_feature/domain/use_case/diary/InsertProductDiaryEntryUseCase.kt
@@ -1,5 +1,6 @@
 package com.gmail.bogumilmecel2.diary_feature.domain.use_case.diary
 
+import com.gmail.bogumilmecel2.common.util.DateUtils.isValidDate
 import com.gmail.bogumilmecel2.common.util.Resource
 import com.gmail.bogumilmecel2.diary_feature.domain.model.diary_entry.ProductDiaryEntry
 import com.gmail.bogumilmecel2.diary_feature.domain.model.diary_entry.ProductDiaryEntryPostRequest
@@ -17,6 +18,8 @@ class InsertProductDiaryEntryUseCase(
         return if (productDiaryEntryPostRequest.weight <= 0) {
             Resource.Error()
         } else if (productDiaryEntryPostRequest.date.isEmpty()) {
+            Resource.Error()
+        } else if(!productDiaryEntryPostRequest.date.isValidDate()) {
             Resource.Error()
         } else {
             diaryRepository.insertDiaryEntry(
