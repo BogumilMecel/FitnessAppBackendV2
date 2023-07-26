40ca681087da02b9272f3cb4ec32e27521991d47
Bogumił Męcel <Bodziowaty6978@gmail.com>
2023-07-26 18:33:28 +0200

    Implement product diary history

--- src/main/kotlin/com/gmail/bogumilmecel2/diary_feature/domain/use_case/diary/GetProductDiaryHistoryUseCase.kt
+++ src/main/kotlin/com/gmail/bogumilmecel2/diary_feature/domain/use_case/diary/GetProductDiaryHistoryUseCase.kt
@@ -1,11 +1,11 @@
 package com.gmail.bogumilmecel2.diary_feature.domain.use_case.diary
 
 import com.gmail.bogumilmecel2.common.util.Resource
-import com.gmail.bogumilmecel2.diary_feature.domain.model.ProductDiarySearchItem
+import com.gmail.bogumilmecel2.diary_feature.domain.model.ProductDiaryHistoryItem
 import com.gmail.bogumilmecel2.diary_feature.domain.repository.DiaryRepository
 
 class GetProductDiaryHistoryUseCase(private val diaryRepository: DiaryRepository) {
-    suspend operator fun invoke(userId: String): Resource<List<ProductDiarySearchItem>> {
+    suspend operator fun invoke(userId: String): Resource<List<ProductDiaryHistoryItem>> {
         return diaryRepository.getProductDiaryHistory(userId = userId)
     }
 }
