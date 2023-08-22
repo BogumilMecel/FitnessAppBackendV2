c01a2c83dbf2596678d360e63cd8bdbe58f485a2
Bogumił Męcel <Bodziowaty6978@gmail.com>
2023-08-22 16:58:49 +0200

    Implemented blocking editing and removing diary entries older than 2 week

--- src/main/kotlin/com/gmail/bogumilmecel2/diary_feature/domain/use_case/diary/EditProductDiaryEntryUseCase.kt
+++ src/main/kotlin/com/gmail/bogumilmecel2/diary_feature/domain/use_case/diary/EditProductDiaryEntryUseCase.kt
@@ -5,19 +5,27 @@ import com.gmail.bogumilmecel2.diary_feature.domain.model.diary_entry.EditProduc
 import com.gmail.bogumilmecel2.diary_feature.domain.repository.DiaryRepository
 import com.gmail.bogumilmecel2.diary_feature.domain.use_case.common.CalculateProductNutritionValuesUseCase
 import com.gmail.bogumilmecel2.diary_feature.domain.use_case.common.GetProductUseCase
+import com.gmail.bogumilmecel2.diary_feature.domain.use_case.common.IsTimestampInTwoWeeksUseCase
 
 class EditProductDiaryEntryUseCase(
     private val diaryRepository: DiaryRepository,
     private val getProductDiaryEntryUseCase: GetProductDiaryEntryUseCase,
     private val getProductUseCase: GetProductUseCase,
-    private val calculateProductNutritionValuesUseCase: CalculateProductNutritionValuesUseCase
+    private val calculateProductNutritionValuesUseCase: CalculateProductNutritionValuesUseCase,
+    private val isTimestampInTwoWeeksUseCase: IsTimestampInTwoWeeksUseCase
 ) {
     suspend operator fun invoke(
         editProductDiaryEntryRequest: EditProductDiaryEntryRequest,
         userId: String
     ): Resource<Unit> = with(editProductDiaryEntryRequest) {
         val productDiaryEntry = getProductDiaryEntryUseCase(productDiaryEntryId = productDiaryEntryId).data ?: return Resource.Error()
+
+        if (productDiaryEntry.userId != userId) return Resource.Error()
+        if (editProductDiaryEntryRequest.newWeight == productDiaryEntry.weight) return Resource.Error()
+        if (!isTimestampInTwoWeeksUseCase(productDiaryEntry.utcTimestamp)) return Resource.Error()
+
         val product = getProductUseCase(productId = productDiaryEntry.productId).data ?: return Resource.Error()
+
         val newNutritionValues = calculateProductNutritionValuesUseCase(
             product = product,
             weight = newWeight
--- src/main/kotlin/com/gmail/bogumilmecel2/diary_feature/domain/use_case/recipe/EditRecipeDiaryEntryUseCase.kt
+++ src/main/kotlin/com/gmail/bogumilmecel2/diary_feature/domain/use_case/recipe/EditRecipeDiaryEntryUseCase.kt
@@ -3,12 +3,14 @@ package com.gmail.bogumilmecel2.diary_feature.domain.use_case.recipe
 import com.gmail.bogumilmecel2.common.util.Resource
 import com.gmail.bogumilmecel2.diary_feature.domain.model.EditRecipeDiaryEntryRequest
 import com.gmail.bogumilmecel2.diary_feature.domain.repository.DiaryRepository
+import com.gmail.bogumilmecel2.diary_feature.domain.use_case.common.IsTimestampInTwoWeeksUseCase
 
 class EditRecipeDiaryEntryUseCase(
     private val diaryRepository: DiaryRepository,
     private val getRecipeDiaryEntryUseCase: GetRecipeDiaryEntryUseCase,
     private val getRecipeUseCase: GetRecipeUseCase,
-    private val calculateRecipeNutritionValuesUseCase: CalculateRecipeNutritionValuesUseCase
+    private val calculateRecipeNutritionValuesUseCase: CalculateRecipeNutritionValuesUseCase,
+    private val isTimestampInTwoWeeksUseCase: IsTimestampInTwoWeeksUseCase
 ) {
     suspend operator fun invoke(
         editRecipeDiaryEntryRequest: EditRecipeDiaryEntryRequest,
@@ -17,8 +19,8 @@ class EditRecipeDiaryEntryUseCase(
         val recipeDiaryEntry = getRecipeDiaryEntryUseCase(recipeDiaryEntryId = recipeDiaryEntryId).data ?: return Resource.Error()
 
         if (recipeDiaryEntry.userId != userId) return Resource.Error()
-
         if (recipeDiaryEntry.servings == newServings) return Resource.Error()
+        if (!isTimestampInTwoWeeksUseCase(recipeDiaryEntry.utcTimestamp)) return Resource.Error()
 
         val recipe = getRecipeUseCase(recipeId = recipeDiaryEntry.recipeId).data ?: return Resource.Error()
 
