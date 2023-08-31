019cc803f70de13a35163ac0289d7b3318b36003
Bogumił Męcel <Bodziowaty6978@gmail.com>
2023-08-31 06:57:20 +0200

    Remove calculating calories during editing diary entries

--- src/main/kotlin/com/gmail/bogumilmecel2/diary_feature/domain/use_case/diary/EditProductDiaryEntryUseCase.kt
+++ src/main/kotlin/com/gmail/bogumilmecel2/diary_feature/domain/use_case/diary/EditProductDiaryEntryUseCase.kt
@@ -2,42 +2,29 @@ package com.gmail.bogumilmecel2.diary_feature.domain.use_case.diary
 
 import com.gmail.bogumilmecel2.common.util.CustomDateUtils
 import com.gmail.bogumilmecel2.common.util.Resource
-import com.gmail.bogumilmecel2.diary_feature.domain.model.diary_entry.EditProductDiaryEntryRequest
+import com.gmail.bogumilmecel2.diary_feature.domain.model.diary_entry.ProductDiaryEntry
 import com.gmail.bogumilmecel2.diary_feature.domain.repository.DiaryRepository
-import com.gmail.bogumilmecel2.diary_feature.domain.use_case.common.CalculateProductNutritionValuesUseCase
-import com.gmail.bogumilmecel2.diary_feature.domain.use_case.common.GetProductUseCase
 import com.gmail.bogumilmecel2.diary_feature.domain.use_case.common.IsTimestampInTwoWeeksUseCase
 
 class EditProductDiaryEntryUseCase(
     private val diaryRepository: DiaryRepository,
     private val getProductDiaryEntryUseCase: GetProductDiaryEntryUseCase,
-    private val getProductUseCase: GetProductUseCase,
-    private val calculateProductNutritionValuesUseCase: CalculateProductNutritionValuesUseCase,
     private val isTimestampInTwoWeeksUseCase: IsTimestampInTwoWeeksUseCase
 ) {
     suspend operator fun invoke(
-        editProductDiaryEntryRequest: EditProductDiaryEntryRequest,
+        productDiaryEntry: ProductDiaryEntry,
         userId: String
-    ): Resource<Unit> = with(editProductDiaryEntryRequest) {
-        val productDiaryEntry = getProductDiaryEntryUseCase(productDiaryEntryId = productDiaryEntryId).data ?: return Resource.Error()
-
-        if (productDiaryEntry.userId != userId) return Resource.Error()
-        if (editProductDiaryEntryRequest.newWeight == productDiaryEntry.weight) return Resource.Error()
-        if (!isTimestampInTwoWeeksUseCase(productDiaryEntry.utcTimestamp)) return Resource.Error()
-
-        val product = getProductUseCase(productId = productDiaryEntry.productId).data ?: return Resource.Error()
-
-        val newNutritionValues = calculateProductNutritionValuesUseCase(
-            product = product,
-            weight = newWeight
+    ): Resource<Unit> = with(productDiaryEntry) {
+        val originalProductDiaryEntry = getProductDiaryEntryUseCase(
+            productDiaryEntryId = id
         ).data ?: return Resource.Error()
 
+        if (originalProductDiaryEntry.userId != userId) return Resource.Error()
+        if (productDiaryEntry.weight == originalProductDiaryEntry.weight) return Resource.Error()
+        if (!isTimestampInTwoWeeksUseCase(originalProductDiaryEntry.utcTimestamp)) return Resource.Error()
+
         return diaryRepository.editProductDiaryEntry(
-            productDiaryEntry = productDiaryEntry.copy(
-                nutritionValues = newNutritionValues,
-                weight = newWeight,
-                lastEditedUtcTimestamp = CustomDateUtils.getCurrentUtcTimestamp()
-            ),
+            productDiaryEntry = productDiaryEntry.copy(lastEditedUtcTimestamp = CustomDateUtils.getCurrentUtcTimestamp()),
             userId = userId
         )
     }
