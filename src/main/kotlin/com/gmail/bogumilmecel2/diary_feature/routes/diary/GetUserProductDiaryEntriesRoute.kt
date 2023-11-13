8bfaab30b3afe734d87c720317947265541d55e0
Bogumił Męcel <Bodziowaty6978@gmail.com>
2023-11-13 07:03:46 +0100

    added get user product diary entries route

---
+++ src/main/kotlin/com/gmail/bogumilmecel2/diary_feature/routes/diary/GetUserProductDiaryEntriesRoute.kt
@@ -0,0 +1,24 @@
+package com.gmail.bogumilmecel2.diary_feature.routes.diary
+
+import com.gmail.bogumilmecel2.common.util.extensions.getUserId
+import com.gmail.bogumilmecel2.common.util.extensions.handleResource
+import com.gmail.bogumilmecel2.diary_feature.domain.use_case.diary.GetUserProductDiaryEntriesUseCase
+import io.ktor.server.application.*
+import io.ktor.server.auth.*
+import io.ktor.server.routing.*
+
+fun Route.configureGetUserProductDiaryEntriesRoute(getUserProductDiaryEntriesUseCase: GetUserProductDiaryEntriesUseCase) {
+    authenticate {
+        get("/product_diary") {
+            call.run {
+                getUserId()?.let { userId ->
+                    call.handleResource(
+                        resource = getUserProductDiaryEntriesUseCase(
+                            userId = userId,
+                        )
+                    )
+                }
+            }
+        }
+    }
+}
