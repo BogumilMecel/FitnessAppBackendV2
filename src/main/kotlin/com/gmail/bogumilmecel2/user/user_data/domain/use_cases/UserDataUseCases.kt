dec7220307bfe67f2c3d0101bfb6b6b633ece795
Bogumił Męcel <Bodziowaty6978@gmail.com>
2023-11-17 21:06:14 +0100

    Add get user recipes route

--- src/main/kotlin/com/gmail/bogumilmecel2/user/user_data/domain/use_cases/UserDataUseCases.kt
+++ src/main/kotlin/com/gmail/bogumilmecel2/user/user_data/domain/use_cases/UserDataUseCases.kt
@@ -3,11 +3,13 @@ package com.gmail.bogumilmecel2.user.user_data.domain.use_cases
 import com.gmail.bogumilmecel2.diary_feature.domain.use_case.diary.GetUserProductDiaryEntriesUseCase
 import com.gmail.bogumilmecel2.diary_feature.domain.use_case.diary.GetUserRecipeDiaryEntriesUseCase
 import com.gmail.bogumilmecel2.diary_feature.domain.use_case.product.GetUserProductsUseCase
+import com.gmail.bogumilmecel2.diary_feature.domain.use_case.recipe.GetUserRecipesUseCase
 
 data class UserDataUseCases(
     val saveNutritionValuesUseCase: SaveNutritionValuesUseCase,
     val handleUserInformationUseCase: HandleUserInformationUseCase,
     val getUserProductDiaryEntriesUseCase: GetUserProductDiaryEntriesUseCase,
     val getUserRecipeDiaryEntriesUseCase: GetUserRecipeDiaryEntriesUseCase,
-    val getUserProductsUseCase: GetUserProductsUseCase
+    val getUserProductsUseCase: GetUserProductsUseCase,
+    val getUserRecipesUseCase: GetUserRecipesUseCase
 )
