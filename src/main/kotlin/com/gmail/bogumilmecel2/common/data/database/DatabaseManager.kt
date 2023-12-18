9545011fd749d0ecfdd622006ee56b79c7e72565
Bogumił Męcel <bogumilmecel2@gmail.com>
2023-12-18 16:55:52 +0100

    Organize database manager

--- src/main/kotlin/com/gmail/bogumilmecel2/common/data/database/DatabaseManager.kt
+++ src/main/kotlin/com/gmail/bogumilmecel2/common/data/database/DatabaseManager.kt
@@ -1,5 +1,13 @@
 package com.gmail.bogumilmecel2.common.data.database
 
+import com.gmail.bogumilmecel2.authentication.domain.model.user.UserDto
+import com.gmail.bogumilmecel2.diary_feature.domain.model.diary_entry.ProductDiaryEntryDto
+import com.gmail.bogumilmecel2.diary_feature.domain.model.product.ProductDto
+import com.gmail.bogumilmecel2.diary_feature.domain.model.recipe.RecipeDiaryEntryDto
+import com.gmail.bogumilmecel2.diary_feature.domain.model.recipe.RecipeDto
+import com.gmail.bogumilmecel2.diary_feature.price_feature.domain.model.PriceDto
+import com.gmail.bogumilmecel2.user.log.domain.model.LogEntryDto
+import com.gmail.bogumilmecel2.user.weight.domain.model.WeightEntryDto
 import org.litote.kmongo.coroutine.CoroutineCollection
 import org.litote.kmongo.coroutine.coroutine
 import org.litote.kmongo.reactivestreams.KMongo
@@ -11,11 +19,41 @@ class DatabaseManager {
     private val password = System.getenv("MONGO_PW")
 
     // database
-    var client = KMongo.createClient(
+    private val client = KMongo.createClient(
         connectionString = "mongodb+srv://FitnessApp:$password@cluster0.e8iazl3.mongodb.net/fitness_app?retryWrites=true&w=majority"
     ).coroutine.getDatabase(databaseName)
 
-    suspend inline fun <reified T: Any> listIndexes(collection: CoroutineCollection<T>) {
-        println("${T::class.java.name} indexes: ${collection.listIndexes<Any>().toList()}")
+    fun getProductDiaryCollection(): CoroutineCollection<ProductDiaryEntryDto> = client.getCollection("diary_collection")
+    fun getRecipeDiaryCollection(): CoroutineCollection<RecipeDiaryEntryDto> = client.getCollection("recipe_diary_collection")
+    fun getRecipeCollection(): CoroutineCollection<RecipeDto> = client.getCollection("recipe_collection")
+    fun getProductCollection(): CoroutineCollection<ProductDto> = client.getCollection("product_collection")
+    fun getUserCollection(): CoroutineCollection<UserDto> = client.getCollection("user_collection")
+    fun getWeightCollection(): CoroutineCollection<WeightEntryDto> = client.getCollection("weight_collection")
+    fun getLogEntryCollection(): CoroutineCollection<LogEntryDto> = client.getCollection("log_entry_collection")
+    fun getPriceCollection(): CoroutineCollection<PriceDto> = client.getCollection("price_collection")
+
+    suspend fun listAllIndexes() {
+        listIndexes(getProductDiaryCollection())
+        listIndexes(getRecipeDiaryCollection())
+        listIndexes(getRecipeCollection())
+        listIndexes(getProductCollection())
+        listIndexes(getUserCollection())
+        listIndexes(getWeightCollection())
+        listIndexes(getLogEntryCollection())
+        listIndexes(getPriceCollection())
     }
+
+    private suspend inline fun <reified T: Any> listIndexes(collection: CoroutineCollection<T>) {
+        println("${T::class.java.simpleName} indexes: ${collection.listIndexes<Any>().toList()}")
+    }
+
+    suspend fun <T: Any> removeIndex(
+        collection: CoroutineCollection<T>,
+        indexName: String
+    ) = collection.dropIndex(indexName = indexName)
+
+    suspend fun <T: Any> createIndex(
+        collection: CoroutineCollection<T>,
+        key: String
+    ) = collection.createIndex(key = key)
 }
