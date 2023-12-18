7cf5273b8621d19497f833343744ba4a72a7ff2a
Bogumił Męcel <bogumilmecel2@gmail.com>
2023-12-18 16:55:51 +0100

    Add list indexes function

--- src/main/kotlin/com/gmail/bogumilmecel2/common/data/database/DatabaseManager.kt
+++ src/main/kotlin/com/gmail/bogumilmecel2/common/data/database/DatabaseManager.kt
@@ -1,5 +1,6 @@
 package com.gmail.bogumilmecel2.common.data.database
 
+import org.litote.kmongo.coroutine.CoroutineCollection
 import org.litote.kmongo.coroutine.coroutine
 import org.litote.kmongo.reactivestreams.KMongo
 
@@ -7,11 +8,14 @@ class DatabaseManager {
 
     // config
     private val databaseName = "fitness_app"
-    private val username = System.getenv("MONGO_USER")
     private val password = System.getenv("MONGO_PW")
 
     // database
     var client = KMongo.createClient(
         connectionString = "mongodb+srv://FitnessApp:$password@cluster0.e8iazl3.mongodb.net/fitness_app?retryWrites=true&w=majority"
     ).coroutine.getDatabase(databaseName)
+
+    suspend inline fun <reified T: Any> listIndexes(collection: CoroutineCollection<T>) {
+        println("${T::class.java.name} indexes: ${collection.listIndexes<Any>().toList()}")
+    }
 }
