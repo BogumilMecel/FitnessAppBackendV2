9d5a46e8e278ce8e137be7ca99579c5db7b29035
Bogumił Męcel <bogumilmecel2@gmail.com>
2023-12-18 16:55:58 +0100

    Changes to insert product diary entry use case

--- src/main/kotlin/com/gmail/bogumilmecel2/diary_feature/domain/use_case/diary/InsertProductDiaryEntryUseCase.kt
+++ src/main/kotlin/com/gmail/bogumilmecel2/diary_feature/domain/use_case/diary/InsertProductDiaryEntryUseCase.kt
@@ -1,18 +1,17 @@
 package com.gmail.bogumilmecel2.diary_feature.domain.use_case.diary
 
-import com.gmail.bogumilmecel2.common.domain.constants.Constants
 import com.gmail.bogumilmecel2.common.util.CustomDateUtils
-import com.gmail.bogumilmecel2.common.util.CustomDateUtils.isValidDate
 import com.gmail.bogumilmecel2.common.util.Resource
 import com.gmail.bogumilmecel2.diary_feature.domain.model.diary_entry.ProductDiaryEntry
 import com.gmail.bogumilmecel2.diary_feature.domain.model.diary_entry.ProductDiaryEntryPostRequest
 import com.gmail.bogumilmecel2.diary_feature.domain.repository.DiaryRepository
 import com.gmail.bogumilmecel2.diary_feature.domain.use_case.common.GetProductUseCase
-import kotlinx.datetime.LocalDate
+import com.gmail.bogumilmecel2.diary_feature.domain.use_case.common.IsDateInValidRangeUseCaseUseCase
 
 class InsertProductDiaryEntryUseCase(
     private val diaryRepository: DiaryRepository,
     private val getProductUseCase: GetProductUseCase,
+    private val isDateInValidRangeUseCaseUseCase: IsDateInValidRangeUseCaseUseCase
 ) {
 
     suspend operator fun invoke(
@@ -22,24 +21,22 @@ class InsertProductDiaryEntryUseCase(
         val product = getProductUseCase(productId = productDiaryEntryPostRequest.productId).data ?: return Resource.Error()
 
         if (weight <= 0) return Resource.Error()
-        if (date.isEmpty()) return Resource.Error()
-        if (!date.isValidDate()) return Resource.Error()
-        if (CustomDateUtils.getDaysFromNow(from = LocalDate.parse(date)) > Constants.Diary.MAXIMUM_MODIFY_DATE) return Resource.Error()
+        if (isDateInValidRangeUseCaseUseCase(date)) return Resource.Error()
 
-        val currentTimestamp = CustomDateUtils.getCurrentUtcTimestamp()
+        val currentDate = CustomDateUtils.getCurrentUtcLocalDateTime()
 
         diaryRepository.insertProductDiaryEntry(
             productDiaryEntry = ProductDiaryEntry(
                 weight = weight,
                 mealName = mealName,
-                utcTimestamp = currentTimestamp,
                 date = date,
                 userId = userId,
                 nutritionValues = nutritionValues,
                 productId = product.id,
                 productName = product.name,
                 productMeasurementUnit = product.measurementUnit,
-                editedUtcTimestamp = currentTimestamp
+                creationDate = currentDate,
+                changeDate = currentDate
             ),
             userId = userId
         )
