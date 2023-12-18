207925fbfa2e59c644bb5ffcf3451a954d5ff1e7
Bogumił Męcel <bogumilmecel2@gmail.com>
2023-12-18 16:55:53 +0100

    Fix for isLengthInRange

--- src/main/kotlin/com/gmail/bogumilmecel2/common/util/extensions/String.kt
+++ src/main/kotlin/com/gmail/bogumilmecel2/common/util/extensions/String.kt
@@ -1,3 +1,3 @@
 package com.gmail.bogumilmecel2.common.util.extensions
 
-fun String.isLengthInRange(maximum: Int, minimum: Int = 0): Boolean = length in (minimum+1)..maximum
+fun String.isLengthInRange(maximum: Int, minimum: Int = 0): Boolean = length in minimum..maximum
