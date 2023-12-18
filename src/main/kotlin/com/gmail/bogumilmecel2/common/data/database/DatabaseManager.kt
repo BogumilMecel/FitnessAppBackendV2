25be92dc3165410e94d8f8550fb1127dea064030
Bogumił Męcel <bogumilmecel2@gmail.com>
2023-12-18 16:55:51 +0100

    Remove unused methods from database manager

--- src/main/kotlin/com/gmail/bogumilmecel2/common/data/database/DatabaseManager.kt
+++ src/main/kotlin/com/gmail/bogumilmecel2/common/data/database/DatabaseManager.kt
@@ -1,8 +1,5 @@
 package com.gmail.bogumilmecel2.common.data.database
 
-import com.gmail.bogumilmecel2.authentication.domain.model.user.User
-import com.gmail.bogumilmecel2.diary_feature.domain.model.product.Product
-import org.litote.kmongo.coroutine.CoroutineCollection
 import org.litote.kmongo.coroutine.coroutine
 import org.litote.kmongo.reactivestreams.KMongo
 
@@ -16,14 +13,5 @@ class DatabaseManager {
     // database
     var client = KMongo.createClient(
         connectionString = "mongodb+srv://FitnessApp:$password@cluster0.e8iazl3.mongodb.net/fitness_app?retryWrites=true&w=majority"
-    ).coroutine
-        .getDatabase(databaseName)
-
-    fun provideUserCollection():CoroutineCollection<User>{
-        return client.getCollection()
-    }
-
-    fun provideProductCollection(): CoroutineCollection<Product>{
-        return client.getCollection()
-    }
+    ).coroutine.getDatabase(databaseName)
 }
