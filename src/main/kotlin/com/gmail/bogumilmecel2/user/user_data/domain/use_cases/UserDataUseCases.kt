21ed3a32c5d6b53377139c1cfc33a343c7f0c805
Bogumił Męcel <bogumilmecel2@gmail.com>
2023-12-18 16:55:45 +0100

    Implement getting user recipes and products

--- src/main/kotlin/com/gmail/bogumilmecel2/user/user_data/domain/use_cases/UserDataUseCases.kt
+++ src/main/kotlin/com/gmail/bogumilmecel2/user/user_data/domain/use_cases/UserDataUseCases.kt
@@ -1,6 +1,9 @@
 package com.gmail.bogumilmecel2.user.user_data.domain.use_cases
 
+import com.gmail.bogumilmecel2.diary_feature.domain.use_case.common.GetUserDiaryItemsUseCase
+
 data class UserDataUseCases(
     val saveNutritionValuesUseCase: SaveNutritionValuesUseCase,
-    val handleUserInformationUseCase: HandleUserInformationUseCase
+    val handleUserInformationUseCase: HandleUserInformationUseCase,
+    val getUserDiaryItemsUseCase: GetUserDiaryItemsUseCase
 )
