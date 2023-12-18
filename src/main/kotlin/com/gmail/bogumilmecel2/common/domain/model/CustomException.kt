9a62c325443be2c4a5b7d04980da0f02ef3ab478
Bogumił Męcel <bogumilmecel2@gmail.com>
2023-12-18 16:55:52 +0100

    Implement returning error messages

--- src/main/kotlin/com/gmail/bogumilmecel2/common/domain/model/CustomException.kt
+++ src/main/kotlin/com/gmail/bogumilmecel2/common/domain/model/CustomException.kt
@@ -1,8 +1,9 @@
 package com.gmail.bogumilmecel2.common.domain.model
 
+import com.github.aymanizz.ktori18n.R
 import io.ktor.http.*
 
 data class CustomException(
-    val exception: Exception = Exception(),
+    val message: R? = null,
     val httpStatusCode: HttpStatusCode = HttpStatusCode.Conflict
 )
